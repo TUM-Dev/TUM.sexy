d8671da3337ac55d315b8aa428050e38f5f3bd36
Syed Husain Mustafa <syedhusainmustafa@gmail.com>
2021-01-07 21:51:04 +0530

    Indentation Correction
    I also removed .DS_Store. Will add I2DL in next pull request if this one is successful.

--- redirect/routes.php
+++ redirect/routes.php
@@ -63,11 +63,11 @@ class Route {
             'description' => 'Automata and Formal Languages',
             'target'      => 'https://www7.in.tum.de/um/courses/auto/ws2021/index.php?category=uebungen',
         ],
-        'bmt' => [
+        'bmt'              => [
             'description' => 'Basic Mathematical Tools for Imaging & Visualization',
             'target'      => 'http://campar.in.tum.de/Chair/TeachingWs11BasicMathTools',
         ],
-        'c' => [
+        'c'                => [
             'description' => 'TUM Online',
             'target'      => 'https://campus.tum.de/tumonline/ee/ui/ca2/app/desktop/#/login',
         ],
