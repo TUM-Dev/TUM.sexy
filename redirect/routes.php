e031b2c0ffeab3aa0770c4bab2646150f5e3b84f
Jan <jan.albers@tutanota.com>
2024-11-10 11:53:58 +0100

    add tum chess club

--- redirect/routes.php
+++ redirect/routes.php
@@ -130,10 +130,14 @@ class Route {
             'description' => 'Liste der Computer Science Lehrstühle',
             'target'      => 'https://www.cs.cit.tum.de/cs/forschung/professuren/',
         ],
+        'chess'      => [
+            'description' => 'TUM Chess Club',
+            'target'      => 'https://tumchessclub.wixsite.com/tumchessclub/',
+        ],
         'cis'                 => [
             'description' => 'Cloud Information Systems',
-            'moodle_id'   => '100537',
-        ],
+            'moodle_id'   => '100537'
+        ]
         'commuteplanner'  => [
             'description'   => 'Automatische Routenplanung von, zu und zwischen TUM Events direkt im Kalendar',
             'target'        => 'https://github.com/OfficialFreak/tum-commute-planner'
